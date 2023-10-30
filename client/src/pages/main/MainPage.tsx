93c5a3935f10df0a4443e9df6f2785dc234a2112
Lavrov Rodion <61391385+Veter-ok@users.noreply.github.com>
2023-10-30 19:55:31 +0300

    Added subjects' card

--- client/src/pages/main/MainPage.tsx
+++ client/src/pages/main/MainPage.tsx
@@ -1,13 +1,21 @@
+import SubjectCard from '../../components/subject-card/SubjectCard'
 import './MainPage.scss'
 
 const MainPage = () => {
-  return (
-	<div className="main-page">
-		<div className="main-page__title">Заголовок</div>
-		<div className="main-page__subject-cards">
+
+	const subjects = ["Русский язык", "Профильная математика", "Физика", "Биология",
+						"Химия", "История", "Обществознание"]
+
+	return (
+		<div className="main-page">
+			<div className="main-page__title">Заголовок</div>
+			<div className="main-page__subject-cards">
+				{subjects.map((value, index) => 
+					<SubjectCard key={index} title={value} progres={index * 10}/>
+				)}
+			</div>
 		</div>
-	</div>
-  )
+	)
 }
 
 export default MainPage
